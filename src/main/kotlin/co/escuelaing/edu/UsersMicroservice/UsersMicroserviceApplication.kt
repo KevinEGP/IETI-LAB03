a566f8cef014408c6c0c1415673ea0e1088cf770
nontoa <nicolasnc010@gmail.com>
2022-01-27 17:54:54 -0500

    Base proyecto

---
+++ src/main/kotlin/co/escuelaing/edu/UsersMicroservice/UsersMicroserviceApplication.kt
@@ -0,0 +1,11 @@
+package co.escuelaing.edu.UsersMicroservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UsersMicroserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<UsersMicroserviceApplication>(*args)
+}
